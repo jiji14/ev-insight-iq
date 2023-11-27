eff34f7dbc187e92c55e38fe9bb8b2571a23e365
Jiji14 <jijeong0114@gmail.com>
2023-11-27 12:07:48 -0800

    change tableau url

--- src/Tableau/ChargingInfrastructure.js
+++ src/Tableau/ChargingInfrastructure.js
@@ -6,9 +6,27 @@ function ChargingInfrastructure() {
   const vizContainer = useRef(null);
 
   const initTableau = () => {
-    const vizUrl =
-      "https://public.tableau.com/views/RegionalSampleWorkbook/Storms";
-    let viz = new window.tableau.Viz(vizContainer.current, vizUrl);
+    const vizUrl = "https://public.tableau.com/views/inf_dash/inf_dash";
+    const options = {
+      height: window.innerHeight - 250,
+      width: window.innerWidth - 130,
+      hideToolbar: true,
+    };
+    let viz = new window.tableau.Viz(vizContainer.current, vizUrl, options);
+
+    function autoResize() {
+      const width = window.innerWidth - 130;
+      const height = window.innerHeight - 250;
+
+      viz.setFrameSize(width, height);
+    }
+
+    window.addEventListener("resize", () => {
+      console.log(
+        `Resizing the window: ${window.innerHeight}, ${window.innerWidth}`
+      );
+      autoResize();
+    });
   };
 
   useEffect(() => {
--- src/Tableau/RegionalDetail.js
+++ src/Tableau/RegionalDetail.js
@@ -7,8 +7,27 @@ function RegionalDetail() {
 
   const initTableau = () => {
     const vizUrl =
-      "https://public.tableau.com/views/RegionalSampleWorkbook/Storms";
-    let viz = new window.tableau.Viz(vizContainer.current, vizUrl);
+      "https://public.tableau.com/views/ev-insight-region/Sheet1?:language=en-US&:display_count=n&:origin=viz_share_link";
+    const options = {
+      height: window.innerHeight - 250,
+      width: window.innerWidth - 130,
+      hideToolbar: true,
+    };
+    let viz = new window.tableau.Viz(vizContainer.current, vizUrl, options);
+
+    function autoResize() {
+      const width = window.innerWidth - 130;
+      const height = window.innerHeight - 250;
+
+      viz.setFrameSize(width, height);
+    }
+
+    window.addEventListener("resize", () => {
+      console.log(
+        `Resizing the window: ${window.innerHeight}, ${window.innerWidth}`
+      );
+      autoResize();
+    });
   };
 
   useEffect(() => {
